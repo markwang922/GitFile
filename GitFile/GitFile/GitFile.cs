d50886f0a6af7be6167db3b90d03ee29e0dbf8ed
markwang922 <Mark.Wang@quest.com>
2020-07-10 15:57:30 +0800

    devide to 2 classes

--- GitFile/GitFile/GitFile.cs
+++ GitFile/GitFile/GitFile.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.IO;
 
 namespace GitFileNS
 {
     class GitFile
     {
-        static void Main(string[] args)
+        public void FileWrite(FileStream f)
         {
-            Console.WriteLine("Hello World!");
+            for (int i=1; i <=20; i++)
+            {
+                f.WriteByte((byte)i);
+            }
+        }
+
+        public void FileRead(FileStream f)
+        {
+            for (int i=0; i <=20; i++)
+            {
+                Console.Write(f.ReadByte() + "-> ");
+            }
+        }
+    }
+
+    class Test
+    {
+        public static void Main(string[] args)
+        {
+            FileStream f = new FileStream("test2.txt", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            GitFile gTest = new GitFile();
+
+            gTest.FileWrite(f);
+            f.Position = 0;
+            gTest.FileRead(f);
+
+            f.Close();
+
+            Console.ReadKey();
         }
     }
 }
